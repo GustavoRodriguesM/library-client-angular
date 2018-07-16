2cf9a37430f93add310c67198e62b507dfb3833f
Gustavo Rodrigues <gusttavorodriguesm@gmail.com>
2018-07-16 15:53:38 -0700

    Create BookListComponent

---
+++ src/app/books/book-list/book-list.component.ts
@@ -0,0 +1,20 @@
+import { BookService } from './../book.service';
+import { Book } from './../book';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  books: Book[] = [];
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit() {
+    this.bookService.findAll().subscribe( data => {console.log(data); this.books = data._embedded.books;});
+  }
+
+}
