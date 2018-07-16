e2fd41c5382282d6fac8effb7d9f716dc2031624
Gustavo Rodrigues <gusttavorodriguesm@gmail.com>
2018-07-16 08:54:26 -0700

    Create PhotoComponent and UtilsModule / on app.module.ts: importing utils.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { UtilsModule } from './utils/utils.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UtilsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
