b04e7edf4d83a914bffc434e00bbf4b0092a04b7
Gustavo Rodrigues <gusttavorodriguesm@gmail.com>
2018-07-16 17:36:51 -0700

    BookShowComponent now use ActivatedRoute to get ID by URI

--- src/app/books/book-show/book-show.component.ts
+++ src/app/books/book-show/book-show.component.ts
@@ -1,19 +1,30 @@
-import { BookService } from './../book.service';
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from '@angular/router';
+
+import { BookService } from './../book.service';
 import { Book } from '../book';
 
 @Component({
     selector: 'lib-book-show',
-    templateUrl: './book-show.component.html'
+    templateUrl: './book-show.component.html',
+    styleUrls: ['./book-show.component.css']
 })
 export class BookShowComponent implements OnInit {
     book: Book = new Book();
 
     constructor (
-        private bookService: BookService
+        private bookService: BookService,
+        private activatedRoute: ActivatedRoute
     ) {}
 
     ngOnInit(): void{
-        this.bookService.findById("1").subscribe( data => {this.book = data});      
+        const id = this.activatedRoute.snapshot.params.id;
+        this.findById(id);
+    }
+
+    findById(id: string) {
+        this.bookService
+            .findById(id)
+            .subscribe( data => { this.book = data });
     }
 }
