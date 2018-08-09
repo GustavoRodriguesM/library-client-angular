1b0862febad5b94c6114e2e7c3f9538791ec2220
Gustavo Rodrigues Martinez <gusttavorodriguesm@gmail.com>
2018-08-09 11:47:44 -0700

    Add 'not-found' route

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,5 +1,5 @@
-import { AuthorUpdateResolver } from './authors/author-update/author-update.resolver';
-import { AuthorUpdateComponent } from './authors/author-update/author-update.component';
+import { AuthorUpdateResolver } from "./authors/author-update/author-update.resolver";
+import { AuthorUpdateComponent } from "./authors/author-update/author-update.component";
 import { AuthorCreateComponent } from "./authors/author-create/author-create.component";
 import { AdminOnlyGuard } from "./core/auth/admin-only.guard";
 import { BookCreateComponent } from "./books/book-create/book-create.component";
@@ -81,8 +81,12 @@ const routes: Routes = [
     canActivate: [AdminOnlyGuard]
   },
   {
-    path: "**",
+    path: "not-found",
     component: NotFoundComponent
+  },
+  {
+    path: "**",
+    redirectTo: "not-found"
   }
 ];
 
